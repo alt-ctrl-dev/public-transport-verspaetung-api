e55cc1c6ad666e206ac747020d7e882bf15334c9
Coutinho, Reuben K <reuben.coutinho@accenture.com>
2019-03-15 22:08:29 +1100

    Refactor: Listening to environment port or defaults to 8081

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fastify = require("./app")({
 
 const start = async () => {
 	try {
-		let address = await fastify.listen(8081, "0.0.0.0");
+		let address = await fastify.listen(process.env.PORT || 8081, "0.0.0.0");
 		fastify.log.info(`server listening on ${address}`);
 	} catch (err) {
 		fastify.log.error(err);
